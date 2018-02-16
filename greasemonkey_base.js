b2e7f3da0044028d03f735088836bc0292aa3373
Wolf Pauw <wolf.pauw@dpa-mediatec.com>
2018-02-16 16:56:42 +0100

    Added Metadata for Greasemonkey

---
+++ greasemonkey_base.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     ClipCollector
+// @version  0.1
+// @grant    none
+// @description   Collecting clip data for PR people
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @icon          https://monitor.buzzrank.de/media/images/oauth/logo.png
+// @include       https://*instagram.com*
+// ==/UserScript==
