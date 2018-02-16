f1a2ce4068f258037cf8dbf839c8a1a04d6c192a
Jan-Hendrik Oldag <jan@MacBook-135.local>
2018-02-16 17:26:12 +0100

    PHP-SErver

--- greasemonkey_base.js
+++ greasemonkey_base.js
@@ -4,6 +4,7 @@
 // @grant    none
 // @description   Collecting clip data for PR people
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require       http://localhost:8001/interactions.php
 // @icon          https://monitor.buzzrank.de/media/images/oauth/logo.png
 // @include       https://*instagram.com*
 // ==/UserScript==
